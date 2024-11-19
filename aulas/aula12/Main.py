9b58ab0d2ea678ecfbc89e06d46874d5cecf2246
Samuel Amate <amatesamuel1@gmail.com>
2024-11-19 15:17:30 -0300

    aula12

---
+++ aulas/aula12/Main.py
@@ -0,0 +1,28 @@
+import Pessoa
+import os
+
+os.system('cls')
+
+res = 0
+def pergunta():
+    res = int(input("Deseja cadastrar uma nova pessoa? 1 - SIM ou 0 - NÂO:"))
+    return res
+
+cadastro = []
+
+res = pergunta()
+while(res == 1):
+    nome = str(input("Digite o nome da pessoa: "))
+    idade = int(input("Digite a idade da pessoa: "))
+    cargo = str(input("Digite o cargo da pessoa: "))
+    salario = float(input("Digite o salario da pessoa: "))
+    
+    cadastro.append(Pessoa.Pessoa(nome,idade,cargo,salario))
+    pergunta()
+
+def mostrar():
+    print("{:<4}{:<10}{:<7}{:<10}{:<7}"
+          .format("Nº","Nome","Idade","Cargo","Salário"))
+    for x in cadastro:
+        print("{:<4}{:<10}{:<7}{:<10}{:<7}"
+              .format(x,x.nome,x.idade,x.cargo,x.salario))
